ce718779f57925f0a9a5f3530772a717d8d8ce3a
sunilunnithan <sunilunnithan@gmail.com>
2021-01-12 22:24:21 -0500

    updated linuxsetup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-echo "starting to setup your linux machine"
-
-
-# install all the dev tools 
-sudo yum groupinstall 'Development tools'
-sudo yum install vim tmux
-
-# go to the home directory
-cd ~
-
-# clone the repo
-
-git clone https://github.com/sunilunnithan/dotfiles.git
-
-# copy the dot files
-
-cp ~/dotfiles/vimrc ~/.vimrc
-cp ~/dotfiles/tmux.conf ~/.tmux.conf
+echo "Setting up the vim and tmux environment"
 
 # setup vim 
 mkdir .vim
@@ -36,5 +19,3 @@ vim +PluginInstall +qall
 # setup tmux plugin manager
 mkdir -p .tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# start tmux and press <prefix> I to install all the plugins
